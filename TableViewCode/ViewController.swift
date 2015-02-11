8483ea4d1b85bbfc88c43b1f3f78e219f55f0687
John Regner <john@Johns-MacBook-Pro.local>
2015-02-10 18:40:45 -0600

    TableView displays 1 through 6.
    Added delegate and data source conformance.
    got a simple array set up for the data to display.

--- TableViewCode/ViewController.swift
+++ TableViewCode/ViewController.swift
@@ -2,5 +2,34 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let stuff = [1,2,3,4,5,6]
+
+    override func viewDidLoad() {
+        //else your cell reuse will crash
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+    }
+
 }
 
+
+extension ViewController: UITableViewDataSource {
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stuff.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = String(stuff[indexPath.row])
+        return cell
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+
+}
